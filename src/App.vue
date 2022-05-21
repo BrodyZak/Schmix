2a8f1eac4b38723548d64a70ea2782f76f627433
BrodyZak <31828072+BrodyZak@users.noreply.github.com>
2022-05-20 21:22:09 -0600

    Refactored App.vue into IngredientList #5
    Huge checklist is now held inside the new IngredientList component. It takes the whole array of ingredients from the API from app.vue, lets the user select what they want, then emits those selections back up to app.vue which still handles the API call.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
     <AppHeader/>
-
-    <input class="filter" type="text" v-model="searchIngredients" placeholder="Filter">
-    <button class="styleBtn" @click=getDrinks()>Get Drinks</button>
-
-    <div v-if="ingredients && ingredients.length > 0">
-      <span v-for="(ingredient, index) in filteredIngredients" :key="index">
-        <input :id="ingredient.strIngredient1" :value="ingredient.strIngredient1" type="checkbox" v-model='filters'/>
-        <label :for="ingredient.strIngredient1">{{ingredient.strIngredient1}}</label>
-      </span>
-    </div>
-
+    <IngredientList :ingredients="ingredients" @clicked="getDrinks"/> 
     <DrinkList :drinks="drinks"/> 
-
     <AppFooter/> 
 </template>
 
 <script> 
 import AppHeader from './components/AppHeader'
 import AppFooter from './components/AppFooter'
+import IngredientList from './components/IngredientList' 
 import DrinkList from './components/DrinkList' 
 
 export default {
@@ -26,22 +16,13 @@ export default {
   components: {
     AppHeader, 
     AppFooter,
-    DrinkList,
+    IngredientList,
+    DrinkList
   },
   data(){
     return {
       drinks: [],  
-      showIngredients: true,
       ingredients: [] ,
-      filters: [],
-      searchIngredients: ""  
-    }
-  },
-  computed: {
-    filteredIngredients(){
-      return this.ingredients.filter((ingredient) =>{
-        return ingredient.strIngredient1.toLowerCase().match(this.searchIngredients.toLowerCase())
-      })
     }
   },
   async mounted(){
@@ -57,12 +38,15 @@ export default {
         }
       }
     },
-     async getDrinks(){
-        const res = await fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?i=${this.filters.join(',')}`, this.getOptions())
+
+     async getDrinks(filters){
+        console.log("filters: ", filters)
+        const res = await fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?i=${filters.join(',')}`, this.getOptions())
         console.log("response from call: ", res) 
         const data = await res.json()
         this.drinks =  data.drinks 
     },  
+
     async getIngredients(){
       const res = await fetch('https://the-cocktail-db.p.rapidapi.com/list.php?i=list', this.getOptions())
       const data = await res.json() 
@@ -79,30 +63,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-span{
-  width: 80%; 
-  padding: 5px; 
-}
-.filter{
-  width: 30%; 
-  margin-bottom: 20px; 
-  height: 30px; 
-  border-radius: 5px; 
-  outline: 0; 
-  border: 1px solid grey; 
-  background-color: #f5f5f5;
-}
-.styleBtn{
-  margin: 5px; 
-  height: 35px; 
-  width: 75px; 
-  border-radius: 5px; 
-  outline: 0; 
-  border: 1px solid grey; 
-  background-color: #f5f5f5;
-}
-.styleBtn:hover{
-  filter:brightness(90%); 
 }
 </style>
